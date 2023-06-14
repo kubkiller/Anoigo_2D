a1457e22d81bad138f039462b342deb52b1f2430
Selast <101228861+Selast@users.noreply.github.com>
2023-06-14 17:30:24 +0900

    add_scripts_comment

--- Assets/jh/Menu.cs
+++ Assets/jh/Menu.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    [Header("Menu")]
+    [Header("Menu")] // 메뉴 부분
     public GameObject menu;
     public GameObject[] selection_position;
     public GameObject menu_select;
@@ -13,11 +13,11 @@ public class Menu : MonoBehaviour
     [SerializeField]
     private int M_index = 0;
 
-    public Dictionary<string, string> Itemdic = new Dictionary<string, string>();
+    public Dictionary<string, string> Itemdic = new Dictionary<string, string>(); //아이템 딕셔너리
     [HideInInspector]
-    public List<string> ItemList = new List<string>();
+    public List<string> ItemList = new List<string>(); //아이템 이름 리스트
 
-    [Header("Inventory")]
+    [Header("Inventory")] //인벤토리 부분
     public Text[] Items;
     public GameObject inventory;
     [SerializeField]
@@ -29,8 +29,10 @@ public class Menu : MonoBehaviour
 
     void Start()
     {
+        //아이템 집어넣기
         Itemdic.Add("문 열쇠", "잠겨있는 문을 열 수 있을 듯 하다.");
         Itemdic.Add("물 양동이", "물이 가득 차있다.");
+        //딕셔너리에서 이름만 가져와서 리스트화
         foreach (string item in new List<string>(Itemdic.Keys))
         {
             ItemList.Add(item);
@@ -40,44 +42,47 @@ public class Menu : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
-        {
+        {//C 키 누르면 메뉴 열기
             IsMenu = true;
             menu.SetActive(true);
         }
         if (IsMenu)
         {
             if (!IsInven)
-            {
+            {//인벤이 안열려 있을때 실행
                 if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
+                {//윗키 누르면 인덱스줄여주기(0이 가장 처음)
                     M_index--;
                     if (M_index <= 0) M_index = 0;
                     menu_select.transform.position = selection_position[M_index].transform.position;
                 }
                 if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
+                {//아랫키 누르면 인덱스 늘려주기
                     M_index++;
                     if (M_index >= 3) M_index = 3;
                     menu_select.transform.position = selection_position[M_index].transform.position;
                 }
                 if (Input.GetKeyDown(KeyCode.Z))
-                {
+                {//인덱스 번호에 따라서 다른 UI띄우기
                     if (M_index == 0)
-                    {
+                    {//첫번째는 인벤토리
                         Inventory();
+                    }else if(M_index == 1)
+                    {//두번째는 음량
+                        
                     }
                 }
                 if (Input.GetKeyDown(KeyCode.X))
-                {
+                {//메뉴 끄기
                     M_index = 0;
                     menu.SetActive(false);
                     IsMenu = false;
                 }
             }
             else if (IsInven)
-            {
+            {//인벤토리가 켜져있으면 실행
                 if (Input.GetKeyDown(KeyCode.LeftArrow))
-                {
+                {//메뉴랑 같음
                     In_index--;
                     if (In_index <= 0) In_index = 0;
                     in_select.transform.position = Items[In_index].gameObject.transform.position;
@@ -89,7 +94,7 @@ public class Menu : MonoBehaviour
                     in_select.transform.position = Items[In_index].gameObject.transform.position;
                 }
                 if (Input.GetKeyDown(KeyCode.X))
-                {
+                {//인벤토리 끄기. 끄면서 텍스트 초기화 해주기
                     for (int i = 0; i < ItemList.Count; i++)
                     {
                         Items[i].text = "";
@@ -98,7 +103,7 @@ public class Menu : MonoBehaviour
                     In_index = 0;
                     IsInven = false;
                     inventory.SetActive(false);
-                }
+                }//아이템 설명 텍스트는 항상 실행
                 explan_text.text = Itemdic[ItemList[In_index]];
             }
         }
@@ -106,13 +111,13 @@ public class Menu : MonoBehaviour
 
     private void Inventory()
     {
-        IsInven = true;
+        IsInven = true;//인벤토리 열기
         inventory.SetActive(true);
-        if (ItemList.Count == 0)
+        if (ItemList.Count == 0)//아이템이 없다면 첫번째로 고정
         {
             in_select.transform.position = Items[0].transform.position;
         }
-        else
+        else   //아니라면 아이템 갯수만큼 텍스트를 활성화
         {
             for (int i = 0; i < ItemList.Count; i++)
             {
--- Assets/jh/Save.cs
+++ Assets/jh/Save.cs
@@ -23,28 +23,31 @@ public class Save : MonoBehaviour
     }
     public void SaveData()
     {
+        //데이터 저장
         PlayerData myData = new PlayerData();
+        //데이터 저장하기 전 세이브 데이터 폴더가 없으면 만들기
         if (!Directory.Exists(Application.dataPath + "/SaveData/"))
         {
             Directory.CreateDirectory(Application.dataPath + "/SaveData/");
         }
-
+        //세이브 데이터 폴더 안에 플레이어 데이터(숫자)식의 데이터 파일 저장
         File.WriteAllText(Application.dataPath + "/SaveData/PlayerData"+ 1 +".json", JsonUtility.ToJson(myData));
 
+        //저장하고 바로 불러오기
         myData.GetData();
     }
 
     public void LoadData()
     {
         try
-        {
+        {//세이브가 되어있으면 불러오기
             string jsonData = File.ReadAllText(Application.dataPath + "/SaveData/PlayerData" + 1 + ".json");
             PlayerData data = JsonUtility.FromJson<PlayerData>(jsonData); 
             data.GetData();
         }
         catch (Exception)
         {
-            SaveData();
+            return;
         }
     }
 }
